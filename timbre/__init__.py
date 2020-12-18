ee776373c3b0f0e9ad6bc89ed27bfbaf7f44f305
Matthew Dahmer <matthew.dahmer@gmail.com>
2020-12-18 13:14:11 -0500

    Make further changes for testr comaptibility, add setuptools_scm versioning functionality

--- timbre/__init__.py
+++ timbre/__init__.py
@@ -1,7 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import ska_helpers
 
 from .timbre import load_model_specs, get_full_dtype, get_local_model, c_to_f, f_to_c, setup_model, run_profile
 from .timbre import calc_binary_schedule, create_opt_fun, find_second_dwell, run_state_pairs
+
+__version__ = ska_helpers.get_version(__package__)
 
 
 def test(*args, **kwargs):
